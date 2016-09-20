bf7ac3a0e89f955f50eb1772a8f74dc9fb9e7a4d
skk2142 <skk2142@columbia.edu>
2016-09-19 23:51:26 -0400

    Works for Q1 and exit command works

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <errno.h>
 // int getline(char **, size_t *, FILE *);
 // typedef struct command;
 
@@ -11,10 +12,13 @@
 #define MAXHIST 100
 struct command{
 	char *script_name;
-	char *arguments[MAXARGS];
+	char *arguments[MAXHIST];
 };
 
 struct command *parse_command(char *);
+void run_command(struct command *, char **, char *);
+
+// extern int errno;
 
 int main()
 {
@@ -22,60 +26,30 @@ int main()
 	size_t len = 0;
 	ssize_t read = 0;
 	struct command *cmd;
-	char *history_array[MAXHIST] = {NULL};
-	int hist_count = 0;
-	int head = 0;
+	char *curr_dir = NULL;
+	// char *history_array[MAXHIST] = {NULL};
+	// int hist_count = 0;
+	// int head = 0;
 	while (1) {
 		printf("$");
 		read = getline(&line, &len, stdin);
-		if (read>0){
-			hist_count++;
-			history_array[array_index(head++)] = deep_copy(line);
-		}
+		// if (read>0){
+		// 	hist_count++;
+		// 	history_array[array_index(head++)] = deep_copy(line);
+		// }
 		cmd = parse_command(line);
-		printf("%s",cmd->script_name);
-		int count = 0;
-		char *token;
-		while((token=cmd->arguments[count]))
-		{
-			printf("%d: %s",count, token);
-			count++;
-		}
-		printf("command parsed\n");
-		// printf("%s\n", cmd->script_name);
-		// if (cmd->options)
-		// 	printf("%s\n", cmd->options);
-		// if (cmd->arguments)
-		// 	printf("%s\n", cmd->arguments);
-		// free(cmd->script_name);
-		// free(cmd->arguments);
-		// free(cmd->options);
-		pid_t pid;
-		pid = fork();
-		if (pid < 0) {
-			printf("Fork Failed");
-			return 1;
-		}
-		else if (pid == 0){
-			printf("About to execute the command in child process\n");
-			execv(cmd->arguments[0], cmd->arguments);
-			exit(1);
-		}
-		else {
-			printf("pid in parent: %d", (int) pid);
-			while(pid != wait(0));
-			printf("command finished running");
-			printf("%s\n", "About to free cmd memory");
-			free(cmd);
-			printf("%s\n", "freed cmd memory");			
-		}
-		// token = strtok(line, " ");
-		// printf("%s\n", token);
-		// while ((token = strtok(NULL, " "))!=0)
-		// 	printf("%s\n", token);
-		// printf("size = %zu\n", sizeof(*line));
+		// if (((strcmp(cmd->script_name,"/bin/ls")==0) && ((cmd->arguments[1]==(char *)NULL)) && curr_dir))
+		// {
+		// 	printf("%d\n", strcmp(cmd->script_name,"/bin/ls"));
+		// 	printf("%s\n", curr_dir);
+		// 	cmd->arguments[1] = curr_dir;
+		// 	cmd->arguments[2] = (char *)NULL;
+		// }
+		run_command(cmd, &curr_dir, line);
+		// if (curr_dir)
+		// 	printf("%s\n", curr_dir);
 	};
-	free(line);
+	// free(line);
 	return 0;
 }
 
@@ -85,19 +59,54 @@ struct command *parse_command(char *line)
 {
 	struct command *cmd = (struct command *) malloc(sizeof(struct command));
 	char *token;
+	line = strtok(line, "\n");
 	token = strtok(line, " ");
+	int count = 0;
 	if (token)
 	{
 		cmd->script_name = token;
+		cmd->arguments[count++] = token;
 	}
-	int count = 0;
-	cmd->arguments[count] = token;
-	while ((token = strtok(NULL, " "))!=NULL)
+	while (!((token = strtok(NULL, " "))==NULL))
 	{
-		cmd->arguments[++count] = token;
+		cmd->arguments[count++] = token;
 	}
-	cmd->arguments[++count] = (char *) NULL;
+	cmd->arguments[count++] = (char *)NULL;
 	return(cmd);
+}
+
+void run_command(struct command *cmd, char **curr_dir, char *line)
+{
+	if (strcmp(cmd->script_name,"exit")==0)
+	{
+		free(cmd);
+		free((char*)line);
+		exit(0);
+	}
+	else if (strcmp(cmd->script_name,"cd")==0)
+	{
+		if ((cmd->arguments[1]))
+			*curr_dir = cmd->arguments[1];
+		else
+			perror("Error: Not a valid command");
+	}
+	else
+	{
+		pid_t pid;
+		pid = fork();
+		if (pid < 0) {
+			perror("Error: Fork Failed");
+		}
+		else if (pid == 0){
+			execv(cmd->script_name, cmd->arguments);
+			exit(1);
+		}
+		else {
+			while(pid != wait(0));
+			free(cmd);	
+		}
+
+	}
 }
 
 int array_index(int i){
